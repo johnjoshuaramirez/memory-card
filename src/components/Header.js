f2049528331bd5a6c99d4bd05853c4b85fad56b3
John Joshua Ramirez <johnjoshuaramirez.official@gmail.com>
2023-04-14 00:17:40 -0700

    style: change font family to adventure time

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import styles from '../styles/Header.module.css';
 export default function Header({ currentScore, bestScore }) {
   return (
     <div className={styles.header}>
-      <p>
-        Current Score:<span>{currentScore}</span>
-      </p>
-      <p>
-        Best Score: <span>{bestScore}</span>
-      </p>
+      <h1>Adventure Time</h1>
+      <div className={styles.group}>
+        <p>
+          Current Score:<span>{currentScore}</span>
+        </p>
+        <p>
+          Best Score: <span>{bestScore}</span>
+        </p>
+      </div>
     </div>
   );
 }
